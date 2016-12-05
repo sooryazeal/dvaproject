ab48368476f42d6230f9e244a9ce4c19f5d11b7d
satyajeet.gawas <satyajeet.gawas@gmail.com>
2016-12-04 20:31:42 -0500

    Doctor File added

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 		get :search
 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/doctor/:id', to: 'doctor#show'
 end
