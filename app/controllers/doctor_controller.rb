8bf3144429b5b6d2db4b102eff460654e7205609
satyajeet.gawas <satyajeet.gawas@gmail.com>
2016-12-06 04:12:23 -0500

    last page added

--- app/controllers/doctor_controller.rb
+++ app/controllers/doctor_controller.rb
@@ -1,18 +1,20 @@
-require 'byebug'
+# require 'byebug'
 class DoctorController < ActionController::Base
   protect_from_forgery with: :exception
 
 
   def show
-  	@doctor = PhysicianCompare.search_id(params[:id])
-  	@payments = Payment.get_payments(@doctor.FirstName, @doctor.LastName, @doctor.MiddleName)
+  	# @doctor = PhysicianCompare.search_id(params[:id])
+  	# @payments = Payment.get_payments(@doctor.FirstName, @doctor.LastName, @doctor.MiddleName)
   	# byebug
+    @doc_prop = ["Dr John Doe", "Atlanta, GA", "15+ years experience", "Education : John Hopkins University"]
   	@scorex = 800
   	@payments_recieved = 10000
   	@drugs_prescribed = 5000
   	@rating = 4
   	@payments = [800,600, 500, 400, 350]
-  	@drugs = [10000,7000,5000,3000,2000]
+  	@prescriptions = [10000,7000,5000,3000,2000]
+    @tags = ["cardio","emory"]
   end
 
 end
